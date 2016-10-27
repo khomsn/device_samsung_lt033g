f7aa9684ab4ea43c94ffb798ccfc0632167165bb
ShevT <shev_t@mail.ru>
2016-10-27 22:02:12 +0300

    device: Add wpa_supplicant.conf package

--- device.mk
+++ device.mk
@@ -206,7 +206,6 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
@@ -216,6 +215,7 @@ PRODUCT_PACKAGES += \
     hostapd_default.conf \
     libwpa_client \
     wpa_supplicant \
+    wpa_supplicant.conf \
     libnetcmdiface \
     macloader \
     wifiloader
