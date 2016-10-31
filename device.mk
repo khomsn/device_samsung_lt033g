c8cd51b741945f1997be2aa9279b19e3157b0311
ShevT <shev_t@mail.ru>
2016-10-31 12:09:52 +0300

    device: Update HWUI caches

--- device.mk
+++ device.mk
@@ -271,14 +271,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.texture_cache_size=96 \
     ro.hwui.layer_cache_size=64 \
     ro.hwui.path_cache_size=16 \
+    ro.hwui.texture_cache_flushrate=0.4 \
     ro.hwui.shape_cache_size=4 \
     ro.hwui.gradient_cache_size=2 \
-    ro.hwui.drop_shadow_cache_size=8 \
-    ro.hwui.r_buffer_cache_size=4 \
-    ro.hwui.text_small_cache_width=2048 \
-    ro.hwui.text_small_cache_height=2048 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
     ro.hwui.text_large_cache_width=4096 \
-    ro.hwui.text_large_cache_height=4096 \
+    ro.hwui.text_large_cache_height=2048 \
     ro.hwui.fbo_cache_size=16
 
 # Hwc - not used on cm/aosp
