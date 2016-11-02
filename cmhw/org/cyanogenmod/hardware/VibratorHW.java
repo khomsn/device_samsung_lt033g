1f83047870c7325eede6fdacf2dc984130defb6c
ShevT <shev_t@mail.ru>
2016-11-02 12:42:01 +0300

    cmhw: Update VibratorHW class

--- cmhw/org/cyanogenmod/hardware/VibratorHW.java
+++ cmhw/org/cyanogenmod/hardware/VibratorHW.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2013-2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,56 +16,66 @@
 
 package org.cyanogenmod.hardware;
 
-import org.cyanogenmod.hardware.util.FileUtils;
-
-import java.io.File;
+import org.cyanogenmod.internal.util.FileUtils;
 
 public class VibratorHW {
 
-    private static String LEVEL_PATH = "/sys/class/timed_output/vibrator/pwm_value";
+    private static final String DEFAULT_PATH = "/sys/class/timed_output/vibrator/pwm_default";
+    private static final String LEVEL_PATH = "/sys/class/timed_output/vibrator/pwm_value";
+    private static final String MAX_PATH = "/sys/class/timed_output/vibrator/pwm_max";
+    private static final String MIN_PATH = "/sys/class/timed_output/vibrator/pwm_min";
+    private static final String THRESHOLD_PATH = "/sys/class/timed_output/vibrator/pwm_threshold";
 
     public static boolean isSupported() {
-        File f = new File(LEVEL_PATH);
-        return f.exists();
+        return FileUtils.isFileWritable(LEVEL_PATH) &&
+                FileUtils.isFileReadable(LEVEL_PATH) &&
+                FileUtils.isFileReadable(DEFAULT_PATH) &&
+                FileUtils.isFileReadable(MAX_PATH) &&
+                FileUtils.isFileReadable(MIN_PATH) &&
+                FileUtils.isFileReadable(THRESHOLD_PATH);
     }
 
     public static int getMaxIntensity()  {
-        return 100;
+        try {
+            return Integer.parseInt(FileUtils.readOneLine(MAX_PATH));
+        } catch (NumberFormatException e) {
+            return -1;
+        }
     }
 
     public static int getMinIntensity()  {
-        return 0;
+        try {
+            return Integer.parseInt(FileUtils.readOneLine(MIN_PATH));
+        } catch (NumberFormatException e) {
+            return -1;
+        }
     }
 
     public static int getWarningThreshold()  {
-        /* 100 is the default value on stock */
-        return 100;
+        try {
+            return Integer.parseInt(FileUtils.readOneLine(THRESHOLD_PATH));
+        } catch (NumberFormatException e) {
+            return -1;
+        }
     }
 
     public static int getCurIntensity()  {
-        File f = new File(LEVEL_PATH);
-        String intensity = FileUtils.readOneLine(LEVEL_PATH);
-
-        intensity = intensity.replace("intensity: ", "");
-
-        if(f.exists()) {
-            return Integer.parseInt(intensity);
-        } else {
-            return 0;
+        try {
+            return Integer.parseInt(FileUtils.readOneLine(LEVEL_PATH));
+        } catch (NumberFormatException e) {
+            return -1;
         }
     }
 
     public static int getDefaultIntensity()  {
-        return 50;
+        try {
+            return Integer.parseInt(FileUtils.readOneLine(DEFAULT_PATH));
+        } catch (NumberFormatException e) {
+            return -1;
+        }
     }
 
     public static boolean setIntensity(int intensity)  {
-        File f = new File(LEVEL_PATH);
-
-        if(f.exists()) {
-            return FileUtils.writeLine(LEVEL_PATH, String.valueOf(intensity));
-        } else {
-            return false;
-        }
+        return FileUtils.writeLine(LEVEL_PATH, String.valueOf(intensity));
     }
 }
