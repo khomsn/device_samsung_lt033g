2a88e437ed96fe6ae2743df2e2b31edd1104886f
ShevT <shev_t@mail.ru>
2017-02-20 11:31:33 +0300

    Revert "audio: move audio_effects.conf to vendor"
    This reverts commit 49ecfe4cc05bf03df1405afc718556c6b1c64746.

--- device.mk
+++ device.mk
@@ -54,7 +54,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
 
 # Audio
