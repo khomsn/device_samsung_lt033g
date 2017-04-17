31815da8ea815c01867494228f76b7b26a181d19
ShevT <shev_t@mail.ru>
2017-04-17 14:24:51 +0300

    Revert "lt033g: Temporarily added the Root access"
    This reverts commit 06e449759e663a1abb7b7f56affdaf392a69b12b.

--- device.mk
+++ device.mk
@@ -242,21 +242,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.hsxpa=1 \
     ro.ril.gprsclass=10
 
+# adb
 ADDITIONAL_DEFAULT_PROPERTIES += \
-    persist.sys.usb.config=mtp \
+    persist.service.adb.enable=1 \
+    ro.debuggable=1 \
     ro.debug_level=0x4948
 
-# adb has root
-ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.adb.secure=0 \
-    persist.adb.notify=0 \
-    ro.secure=0 \
-    ro.debuggable=1 \
-    persist.service.adb.enable=1
-
-# root for adb and apps
-ADDITIONAL_BUILD_PROPERTIES += \
-    persist.sys.root_access=3
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
 
 # limit dex2oat threads to improve thermals
 PRODUCT_PROPERTY_OVERRIDES += \
